0375e42250da8cde2434b942d18724b72298b1a9
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-27 15:28:41 -0500

    Adding model to schema

--- gql/schema.js
+++ gql/schema.js
@@ -5,6 +5,11 @@ const typeDefs = gql`
     id: ID
     name: String
     username: String
+    email: String
+    avatar: String
+    description: String
+    password: String
+    createAt: String
   }
 
   type Query {
