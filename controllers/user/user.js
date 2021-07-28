b25bd9a5806a8226fb4c208b9db837b014a7f03a
DreadfulCrazy <lucasospina97@gmail.com>
2021-07-28 00:48:55 -0500

    Adding email and password validation to controller

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -1,6 +1,9 @@
+const { UserInputError } = require('apollo-server')
 const bcryptjs = require('bcryptjs')
 const User = require('../../models/user')
 const { createToken } = require('../../utils/createToken/createToken')
+const { emailValidation } = require('../../utils/validations/emailValidation')
+const { passwordValidation } = require('../../utils/validations/passwordValidation')
 
 const userRegister = async input => {
   const newUser = input
@@ -8,6 +11,15 @@ const userRegister = async input => {
   newUser.email = newUser.email.toLowerCase()
   newUser.username = newUser.username.toLowerCase()
   const { email, username, password } = newUser
+
+  const validateEmail = emailValidation(email)
+  if (!validateEmail) throw new UserInputError('Email is not correct')
+
+  const validatePassword = passwordValidation(password)
+  if (validatePassword.length > 0)
+    throw new UserInputError('There is an error in the password', {
+      errors: validatePassword
+    })
 
   const foundEmail = await User.findOne({ email })
   if (foundEmail) throw new Error('Email Already Exist')
